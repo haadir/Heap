9cd6c3d13a870f343fff5f862d0992533da865a9
Haadi Razzak <hrazzak@usc.edu>
2022-11-28 23:30:38 +0000

    Changes fixed for REGRADE

--- llrec.cpp
+++ llrec.cpp
@@ -6,20 +6,18 @@
 
 void llpivot (Node*& head, Node*& smaller, Node*& larger, int pivot) {
 	if (head == NULL) {
-		smaller = nullptr;
-		larger = nullptr;
+		smaller = NULL;
+		larger = NULL;
 		return;
 	}
 	llpivot(head->next, smaller, larger, pivot);
-	if (head->val > pivot) {
-		head->next = larger;
-		larger = head;
-	}
-	else if (head->val <= pivot) {
+	if (head->val <= pivot) {
 		head->next = smaller;
 		smaller = head;
 	}
 	else {
-		head = NULL;
+		head->next = larger;
+		larger = head;
 	}
+	head = NULL;
 }
